2ab4f945a637e12816057872eb51e1622cae2272
Jakob Möllås <jakob.mollas@gmail.com>
2021-08-10 23:57:42 +0200

    Improve steering, add game over

--- game.js
+++ game.js
@@ -1,18 +1,24 @@
 'use strict'
 
+const tileCount = 20;         // logical game area size
+const tileSize = 20;          // size of each tile, pixels
+
 let canvas;
 let context;
-let playerX = 10;
-let playerY = 10;
-let score = 0;
-let tileCount = 20;         // logical game area size
-let tileSize = 20;          // size of each tile, pixels
-let foodX = Math.floor(Math.random() * tileCount);
-let foodY = Math.floor(Math.random() * tileCount);
-let movementX = 1;             // x/y movement direction, one speed only
-let movementY = 0;
-let tailLength = 5;
+
+let playerX;
+let playerY;
+let hasChangedDirection;
+let score;
+let gameOver;
+let foodX;
+let foodY;
+let movementX;
+let movementY;
+let tailLength;
 let tail = [];
+
+initialize();
 
 window.onload = function () {
     canvas = document.getElementById("game-canvas");
@@ -22,81 +28,122 @@ window.onload = function () {
 }
 
 function mainLoop() {
-    // todo: add game over
+    // --- game logic ---------------------------------------
+    if (!gameOver) {
+        // move
+        hasChangedDirection = false;
+        playerX += movementX;
+        playerY += movementY;
 
-    // *** game logic ***
-    // move
-    playerX += movementX;
-    playerY += movementY;
+        // handle game area wraparound
+        if (playerX < 0) { playerX = tileCount - 1; }
+        if (playerX > tileCount - 1) { playerX = 0; }
+        if (playerY < 0) { playerY = tileCount - 1; }
+        if (playerY > tileCount - 1) { playerY = 0; }
 
-    // handle game area wraparound
-    if (playerX < 0) { playerX = tileCount - 1; }
-    if (playerX > tileCount - 1) { playerX = 0; }
-    if (playerY < 0) { playerY = tileCount - 1; }
-    if (playerY > tileCount - 1) { playerY = 0; }
+        // handle self collision, i.e. game over
+        // (this could be done in rendering as an optimization)
+        for (let t of tail) {
+            if (t.x === playerX && t.y === playerY) {
+                gameOver = true;
+                break;
+            }
+        }
 
-    // todo: handle game over, i.e. collision with self
+        // handle food collision
+        if (foodX === playerX && foodY === playerY) {
+            tailLength++;
+            score++;
 
-    // handle food collision
-    if (foodX === playerX && foodY === playerY) {
-        tailLength++;
-        score++;
+            foodX = Math.floor(Math.random() * tileCount);
+            foodY = Math.floor(Math.random() * tileCount);
+        }
 
-        foodX = Math.floor(Math.random() * tileCount);
-        foodY = Math.floor(Math.random() * tileCount);
+        // add current pos to tail, remove excess tail
+        tail.push({ x: playerX, y: playerY });
+        while (tail.length > tailLength) {
+            tail.shift();
+        }
     }
 
-    // add current pos to tail, remove excess tail
-    tail.push({ x: playerX, y: playerY });
-    while (tail.length > tailLength) {
-        tail.shift();
-    }
-
-    // *** render ***
+    // --- rendering ---------------------------------------
     // background
     context.fillStyle = "black";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     // player/snake
-    for (let i = 0; i < tailLength; i++) {
-        context.fillStyle = i === tailLength - 1 ? "aqua" : "lime";
+    for (let i = 0; i < tail.length; i++) {
+        context.fillStyle = i === tail.length - 1 ? "aqua" : "lime";
 
         let t = tail[i];
         context.fillRect(t.x * tileSize, t.y * tileSize, tileSize - 2, tileSize - 2);
-
-        // Todo: check collision with self -> reset game
     }
 
     // food
     context.fillStyle = "deeppink";
     context.fillRect(foodX * tileSize, foodY * tileSize, tileSize - 2, tileSize - 2);
 
-    // scoring
+    // scoring etc.
+    showGameOver(gameOver);
     showScore(score);
 }
 
 function showScore(score) {
-    let socreboard = document.getElementById("score");
-    socreboard.innerHTML = "Score: " + score;
+    let scoreBoard = document.getElementById("score");
+    scoreBoard.innerHTML = "Score: " + score;
+}
+
+function showGameOver(gameOver) {
+    if (!gameOver) {
+        return;
+    }
+
+    context.font = "7rem consolas";
+    context.fillText("GAME", 70, 180);
+    context.fillText("OVER", 70, 300);
 }
 
 function keyDown(e) {
+    // space
+    if (e.keyCode === 32 && gameOver) { 
+        initialize();
+        return;
+    }
+            
+    // avoid multiple movement changes within one game "tick"
+    if (hasChangedDirection) {
+        return;
+    }
+
     switch (e.keyCode) {
-        // Todo: ensure player cannot move back 180 degrees, just ignore such ket presses
         case 37:    // left arrow
-            movementX = -1; movementY = 0;
+            if (movementX !== 1) { movementX = -1; movementY = 0; hasChangedDirection = true}
             break;
 
         case 38:    // up arrow
-            movementX = 0; movementY = -1;
+            if (movementY !== 1) { movementX = 0; movementY = -1; hasChangedDirection = true}
             break;
 
         case 39:    // right arrow
-            movementX = 1; movementY = 0;
+            if (movementX !== -1) { movementX = 1; movementY = 0; hasChangedDirection = true}
             break;
 
         case 40:    // down arrow
-            movementX = 0; movementY = 1;
+            if (movementY !== -1) { movementX = 0; movementY = 1; hasChangedDirection = true}
             break;
     }
+}
+
+function initialize() {
+    playerX = 10;
+    playerY = 10;
+    hasChangedDirection = false;
+    score = 0;
+    foodX = Math.floor(Math.random() * tileCount);
+    foodY = Math.floor(Math.random() * tileCount);
+    movementX = 1;
+    movementY = 0;
+    tailLength = 5;
+    tail = [];   
+    gameOver = false;
 }
